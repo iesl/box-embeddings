923f067126c0833c49c40d1f2f897b215c0035b8
Dhruvesh Patel <dhruvdplayer@gmail.com>
2020-10-01 09:33:21 -0400

    Broadcasting support in intersection

--- box_embeddings/modules/intersection/intersection.py
+++ box_embeddings/modules/intersection/intersection.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple, Union, Dict, Any, Optional
-from box_embeddings.parameterizations import TBoxTensor
+from box_embeddings.parameterizations import BoxTensor
 import torch
 
 from box_embeddings.common.registrable import Registrable
@@ -8,5 +8,16 @@ from box_embeddings.common.registrable import Registrable
 class Intersection(torch.nn.Module, Registrable):
     """Base class for intersection Layer"""
 
-    def forward(self, left: TBoxTensor, right: TBoxTensor) -> TBoxTensor:
+    def forward(self, left: BoxTensor, right: BoxTensor) -> BoxTensor:
+        # broadcast if necessary
+        # let the = case also be processed
+
+        if len(left.box_shape) >= len(right.box_shape):
+            right.broadcast(left.box_shape)
+        else:
+            left.broadcast(right.box_shape)
+
+        return self._forward(left, right)
+
+    def _forward(self, left: BoxTensor, right: BoxTensor) -> BoxTensor:
         raise NotImplementedError
