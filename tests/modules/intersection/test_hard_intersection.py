50af6b7d262dff61d29e9fb8bef0255c354563f8
Dhruvesh Patel <dhruvdplayer@gmail.com>
2020-10-01 10:07:52 -0400

    Broadcasting support in intersection(hard)

--- tests/modules/intersection/test_hard_intersection.py
+++ tests/modules/intersection/test_hard_intersection.py
@@ -27,3 +27,30 @@ def test_intersection_with_broadcasting() -> None:
     assert (
         res.data.numpy() == hard_intersection(box2, box1).data.numpy()
     ).all()
+
+
+def test_intersection_module() -> None:
+    box1 = BoxTensor(torch.tensor([[[1, 1], [3, 5]], [[1, 1], [3, 3]]]))
+    box2 = BoxTensor(torch.tensor([[[2, 0], [6, 2]], [[3, 2], [4, 4]]]))
+    res = BoxTensor(torch.tensor([[[2, 1], [3, 2]], [[3, 2], [3, 3]]]))
+    assert (
+        res.data.numpy() == HardIntersection()(box1, box2).data.numpy()
+    ).all()
+
+
+def test_intersection_with_broadcasting_module() -> None:
+    box1 = BoxTensor(
+        torch.tensor([[[1, 1], [3, 5]], [[1, 1], [3, 3]]])
+    )  # box_shape (2,2)
+    box2 = BoxTensor(torch.tensor([[2, 0], [6, 2]]))  # box_shape (2,)
+    res = BoxTensor(torch.tensor([[[2, 1], [3, 2]], [[2, 1], [3, 2]]]))
+    assert (
+        res.data.numpy() == HardIntersection()(box1, box2).data.numpy()
+    ).all()
+    box1 = BoxTensor(
+        torch.tensor([[[1, 1], [3, 5]], [[1, 1], [3, 3]]])
+    )  # box_shape (2,2)
+    box2 = BoxTensor(torch.tensor([[2, 0], [6, 2]]))  # box_shape (2,)
+    assert (
+        res.data.numpy() == HardIntersection()(box2, box1).data.numpy()
+    ).all()
