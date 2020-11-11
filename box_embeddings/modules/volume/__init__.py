a74930f0519800a3f6a138dfa46dd72516bbc067
Dhruvesh Patel <dhruvdplayer@gmail.com>
2020-11-11 10:48:34 -0500

    Fixed minor issue with namespace

--- box_embeddings/modules/volume/__init__.py
+++ box_embeddings/modules/volume/__init__.py
@@ -1,2 +1,7 @@
 from .volume import Volume, hard_volume, log_hard_volume, HardVolume
 from .soft_volume import soft_volume, log_soft_volume, SoftVolume
+from .bessel_volume import (
+    bessel_volume_approx,
+    log_bessel_volume_approx,
+    BesselApproxVolume,
+)
