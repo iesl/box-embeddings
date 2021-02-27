c26edb582fc291fb104a8f789c240db59023a26d
Tejas Jils Chheda <tejas@Tejass-MacBook-Pro.local>
2021-02-26 23:51:26 -0500

    Added Tensorflow Box Tensor Parameterization

--- box_embeddings/parameterizations/__init__.py
+++ box_embeddings/parameterizations/__init__.py
@@ -1,5 +1,18 @@
-from .box_tensor import TBoxTensor
-from .box_tensor import BoxTensor
-from .delta_box_tensor import MinDeltaBoxTensor
-from .sigmoid_box_tensor import SigmoidBoxTensor
-from .tanh_box_tensor import TanhBoxTensor
+import logging
+from .. import torch_is_available, tensorflow_is_available
+
+logger = logging.getLogger(__name__)
+
+if torch_is_available():
+    from .box_tensor import TBoxTensor
+    from .box_tensor import BoxTensor
+    from .delta_box_tensor import MinDeltaBoxTensor
+    from .sigmoid_box_tensor import SigmoidBoxTensor
+    from .tanh_box_tensor import TanhBoxTensor
+
+if tensorflow_is_available():
+    from .tf_box_tensor import TFTBoxTensor
+    from .tf_box_tensor import TFBoxTensor
+
+if not torch_is_available() and not tensorflow_is_available():
+    logger.warning("Can't find versions of Pytorch or Tensorflow")
