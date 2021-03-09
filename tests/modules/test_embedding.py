1ffb407368c022e354a9f8a3cf7ae03f3d5047f5
Purujit Goyal <puru072@gmail.com>
2021-03-08 21:20:00 -0500

    Added test for all_boxes property in embedding module

--- tests/modules/test_embedding.py
+++ tests/modules/test_embedding.py
@@ -29,5 +29,13 @@ def test_forward() -> None:
     inputs = torch.tensor([2, 3])
     box_tensor_from_emb = embedding.forward(inputs)
 
-    assert (box_tensor_direct.z == box_tensor_from_emb.z).all()
-    assert (box_tensor_direct.Z == box_tensor_from_emb.Z).all()
+    assert box_tensor_direct == box_tensor_from_emb
+
+
+def test_all_boxes() -> None:
+    embedding = BoxEmbedding(5, 10)
+    box_factory = BoxFactory('mindelta_from_vector')
+    W = embedding.weight
+    box_tensor = box_factory(W)
+    box_tensor_from_emb = embedding.all_boxes
+    assert box_tensor == box_tensor_from_emb
