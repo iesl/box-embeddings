95d75ee24609b4feff6256592388abf0e74c2f44
Purujit Goyal <puru072@gmail.com>
2021-03-30 01:56:44 -0400

    added bounding box test

--- tests/modules/test_embedding.py
+++ tests/modules/test_embedding.py
@@ -39,3 +39,18 @@ def test_all_boxes() -> None:
     box_tensor = box_factory(W)
     box_tensor_from_emb = embedding.all_boxes
     assert box_tensor == box_tensor_from_emb
+
+
+def test_bounding_box() -> None:
+    embedding = BoxEmbedding(5, 10)
+    box_factory = BoxFactory('mindelta_from_vector')
+    W = embedding.weight
+    box_tensor = box_factory(W)
+    z = box_tensor.z
+    Z = box_tensor.Z
+    z_min, _ = z.min(dim=0)
+    Z_max, _ = Z.max(dim=0)
+
+    bounding_box = box_factory.box_subclass.from_zZ(z_min, Z_max)
+    bounding_box_from_emb = embedding.get_bounding_box()
+    assert bounding_box == bounding_box_from_emb
