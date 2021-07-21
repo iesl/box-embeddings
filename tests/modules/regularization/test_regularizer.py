ea3324d985425b18306d39d99f6b81d01c2a3475
Purujit Goyal <puru072@gmail.com>
2021-07-20 23:11:54 -0400

    fixes test for regularizaion

--- tests/modules/regularization/test_regularizer.py
+++ tests/modules/regularization/test_regularizer.py
@@ -1,3 +1,5 @@
+import pytest
+
 from box_embeddings.modules.regularization import BoxRegularizer
 from box_embeddings.parameterizations.box_tensor import BoxTensor
 import torch
@@ -7,6 +9,6 @@ def test_regularizer():
     box = BoxTensor(torch.tensor([[[1, 1], [3, 5]], [[2, 0], [6, 2]]]).float())
     regularizer = BoxRegularizer(weight=0.1)
 
-    expected = 0.0
-    res = regularizer(box)
-    assert res == expected
+    expected = NotImplementedError
+    with pytest.raises(expected):
+        regularizer(box)
