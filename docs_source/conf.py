b9bc6d9e22547b2c55313c1da3e145df90ee2dd8
dhruvdcoder <dhruveshpate@umass.edu>
2021-12-09 23:37:22 -0500

    (Temp) Update config for docs

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -12,8 +12,8 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.graphviz",
     "sphinx.ext.inheritance_diagram",
-#    "sphinx.ext.githubpages", # we don't need this because we will manually place CNAME and .nojekyll
-#    "sphinx.ext.coverage", # don't need this either. We will shift to docstr-coverage
+    #    "sphinx.ext.githubpages", # we don't need this because we will manually place CNAME and .nojekyll
+    #    "sphinx.ext.coverage", # don't need this either. We will shift to docstr-coverage
     "sphinx_multiversion",
     "m2r",
 ]
@@ -34,11 +34,11 @@ html_theme_options = {
     "github_repo": "box-embeddings",
     "github_banner": True,
     "github_button": True,
-    #"description": "Python implementation for box embeddings and box representations",
+    # "description": "Python implementation for box embeddings and box representations",
 }
 
-html_extra_path = ['../docs_source/static']
-html_css_files = ['../custom_t.css']
+html_extra_path = ["../docs_source/static"]
+html_css_files = ["../custom_t.css"]
 add_module_names = False
 
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autoclass_content
@@ -50,10 +50,10 @@ autoapi_dirs = ["../box_embeddings"]
 autoapi_root = "."
 autoapi_options = [
     "members",
-    "inherited-members",
-    "undoc-members",
+    #    "inherited-members",
+    #    "undoc-members",
     "show-inheritance",
-    #"show-module-summary",
+    # "show-module-summary",
 ]
 autoapi_add_toctree_entry = False
 autoapi_keep_files = True
@@ -61,9 +61,12 @@ autoapi_keep_files = True
 # see: https://github.com/data-describe/data-describe/blob/master/docs/source/conf.py
 # and https://github.com/data-describe/data-describe/blob/master/docs/make.py
 # Multiversioning
-smv_tag_whitelist = r"^v\d+\.\d+\.\d+b?\d*$"
-smv_branch_whitelist = r"^.*main$"
+#smv_tag_whitelist = r"^v\d+\.\d+\.\d+b?\d*$"
+smv_tag_whitelist = None
+#smv_branch_whitelist = r"^.*main$"
+smv_branch_whitelist = r"featfix-docs-and-license"
 smv_remote_whitelist = r"^.*$"
+#smv_remote_whitelist = None
 html_sidebars = {
     "**": [
         "about.html",
